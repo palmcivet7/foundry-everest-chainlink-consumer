4aa7d67ec5d71144ecf82154c85da83c238c4d3c
palmcivet7 <palmcivet@proton.me>
2023-08-08 22:42:23 +0100

    import chainlink library

--- src/EverestConsumer.sol
+++ src/EverestConsumer.sol
@@ -7,6 +7,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IEverestConsumer} from "./interfaces/IEverestConsumer.sol";
+import {Chainlink} from "@chainlink/contracts/src/v0.8/Chainlink.sol";
 
 error EverestConsumer__RequestDoesNotExist();
 error EverestConsumer__RevealeeShouldNotBeZeroAddress();
